f5a55e94390cb3c8923aa3db1c3de579af709148
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-10-27 20:47:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+def takeANumber(katzDeliLine, name) 
+  katzDeliLine.push(name)
+  return "Welcome, " + name + ". You are number " + katzDeliLine.size + " in line."
+}
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.size === 0) {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    return "Currently serving " + katzDeliLine.shift() + "."
+  }
+end
+
+def currentLine(katzDeliLine) 
+  var tempString = "The line is currently: "
+  if(katzDeliLine.size === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    for(let i = 0; i < katzDeliLine.size; i++) {
+      tempString += (i + 1) + ". " + katzDeliLine[i]
+      if((i + 1) === katzDeliLine.size) {
+
+      }
+      else {
+        tempString += ", "
+      }
+    }
+    return tempString
+  }
+end
